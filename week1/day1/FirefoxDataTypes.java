314f4cefbd2effb0c87cd3a87a547dc54ecca409
Lalasa-123 <lalasakurli09@gmail.com>
2024-08-12 17:49:00 +0530

    Add files via upload
    Week1 Day1

---
+++ week1/day1/FirefoxDataTypes.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FirefoxDataTypes {
+
+	public static void main(String[] args) {
+		int noOfTabOpen=2;
+		 String searchBox="TestLeaf";
+		 boolean tabsClose=true;
+		 char browserLog='c';
+		 float version=121.34f;
+		 long ph=45678990098765L;
+		 
+		 System.out.println("Tabs Opened in Firefox Browser :" +noOfTabOpen);
+		 System.out.println("TestLeaf Search box :" +searchBox);
+		 System.out.println("Tabs Closed :" +tabsClose + "\n" + "Version of the Firefox :" +version);
+
+	}
+
+}
