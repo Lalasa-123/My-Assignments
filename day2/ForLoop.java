ef25195aeacec8563d6f0609fd5d31a57cb78037
Lalasa-123 <lalasakurli09@gmail.com>
2024-08-13 14:52:11 +0530

    Add files via upload

---
+++ day2/ForLoop.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Print even Numbers
+		
+		int value =20;
+		
+		for(int i=1; i<value; i++)
+		{
+			if(i%2 == 1)
+			{
+				System.out.println("Even Number = " + i);
+			}
+		}
+	}
+
+}
---
+++ day2/IfElsestatements.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+public class IfElsestatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int mark=45;
+if(mark<20)
+{
+System.out.println(" Grade C");
+}
+else if(mark < 40)
+{
+	System.out.println("Grade B");	
+}
+else if(mark<50)
+{
+	System.out.println("Grade A");
+}
+else
+{
+System.out.println("O");
+}
+
+for (int i = 0; i < args.length; i++) {
+	
+}
+	}
+}
